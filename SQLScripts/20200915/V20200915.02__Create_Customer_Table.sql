3d79ad2489208431774e2c2094ffa05fd89fbda9
Your Name <you@example.com>
2020-09-15 01:58:05 +0000

    add sql

---
+++ SQLScripts/20200915/V20200915.02__Create_Customer_Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Customers
+(
+    Id int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255),
+    Phone varchar(255)
+)
---
+++ SQLScripts/20201015/V20201015.01__Delete_Customer_City.sql
@@ -0,0 +1,1 @@
+alter table Customers drop column City
